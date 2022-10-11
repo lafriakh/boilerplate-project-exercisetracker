5d9a7b25243744861fc63d4914e02be4212a2336
Rachid Lafriakh <rachid.lakriach@hse24.de>
2022-10-11 22:18:07 +0100

    save user

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -10,6 +14,57 @@ app.get('/', (req, res) => {
 });
 
 
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+});
+
+const exerciseSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'USER'
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+
+const USER = mongoose.model('USER', userSchema);
+const EXERCISE = mongoose.model('EXERCISE', exerciseSchema);
+
+app.post('/api/users', async (req, res) => {
+  const { username } = req.body;
+
+  const user = new USER({
+    username: username,
+  });
+
+  res.json(await user.save());
+});
+
+app.get('/api/users', async (req, res) => {
+
+});
+
+app.get('/api/users/:_id/exercises', async (req, res) => {
+
+});
+app.get('/api/users/:_id/logs', async (req, res) => {
+
+})
 
 
 
